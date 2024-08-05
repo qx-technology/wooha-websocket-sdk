39b7aef437b11fc049c3afd54b1500ee958c21ad
xuboyue <1207366850@qq.com>
2024-08-05 19:46:37 +0800

    cooding

---
+++ dist/main.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.demo = demo;
+const socket_1 = require("./socket");
+const url = "ws://47.57.236.213:8849/ws";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzaG9wIiwiZXhwIjoxNzMwMTA2NDQ1LCJpYXQiOjE3MjIzMzA0NDUsImp0aSI6IjVkMTMwYTkyZGQ0MzE3ZTFiYWE2NTQ5YjNmNzU0NDgzIn0.QdOiSOjNxMv1sP7MzivqcbNi3bh0AtpU2Y0AGyqauNc";
+class MsgCallback {
+    OnRoomDetail(client, param, message) {
+        const content = message.content;
+        console.log(`房间详情: 在线人数(${content.onlinePeople}), 时间(${new Date()})`);
+    }
+    OnRoomGroupBuying(client, param, message) {
+        const content = message.content;
+        console.log(`房间团购详情: 房间ID(${content.id}), 团购ID(${content.groupBuyingId}), 最大可投票数(${content.maxVoteTickets}), 用户最大可投票数(${content.userMaxVoteTickets}), 当前已投票数(${content.currentVoteTickets}), 投票进度(${content.voteProgress / 100})`);
+    }
+    OnRoomGroupBuyingVote(client, param, message) {
+        const content = message.content;
+        console.log(`房间团购投票: 团购ID(${content.groupBuyingId}), 用户ID(${content.userId}), 投票时间(${content.voteTime}), 投票数(${content.tickets})`);
+    }
+    OnRoomGroupBuyingNextProduct(client, param, message) {
+        const content = message.content;
+        if (content.beginTime === "0001-01-01T00:00:00Z") {
+            console.log(`房间团购下一轮商品: 团购ID(${content.groupBuyingId}), 商品ID(${content.productId}), SKUID(${content.skuId})`);
+        }
+        else {
+            console.log(`房间团购下一轮商品: 开始时间(${content.beginTime})`);
+        }
+    }
+    OnRoomGroupBuyingStart(client, param, message) {
+        console.log("房间团购开始");
+    }
+    OnRoomGroupBuyingLotteryOpening(client, param, message) {
+        console.log("房间团购开奖中");
+    }
+    OnRoomGroupBuyingWinning(client, param, message) {
+        const content = message.content;
+        console.log(`房间团购用户中奖: 用户ID(${content.winnerUserId}), 奖品可领奖时间(${content.prizeCollectionTime}), 竞拍ID(${content.auctionId})`);
+    }
+    OnRoomGroupBuyingBiddingCounteroffer(client, param, message) {
+        console.log("房间团购竞拍还价所有人");
+    }
+    OnRoomGroupBuyingBiddingDeal(client, param, message) {
+        console.log("房间团购竞拍成交");
+    }
+    OnRoomGroupBuyingBiddingBuyerInitiatesOffer(client, param, message) {
+        console.log("房间团购竞拍买家发起报价");
+    }
+    OnRoomGroupBuyingBiddingSellerReceivesOffer(client, param, message) {
+        console.log("房间团购竞拍买家收到报价");
+    }
+    OnRoomGroupBuyingBiddingSellerCounteroffer(client, param, message) {
+        console.log("房间团购竞拍买家收到还价");
+    }
+    OnRoomGroupBuyingBiddingBuyerOfferRejected(client, param, message) {
+        console.log("房间团购竞拍买家报价被拒");
+    }
+}
+function main() {
+    demo();
+}
+function demo() {
+    const client = (0, socket_1.newClient)(new MsgCallback(), token, true);
+    client.start();
+    client.enterRoom("1");
+}
+if (require.main === module) {
+    main();
+}
+//# sourceMappingURL=main.js.map
---
+++ dist/socket.js
@@ -0,0 +1,361 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ClientProvider = exports.RequestInfo = void 0;
+exports.configSite = configSite;
+exports.useHttps = useHttps;
+exports.useWss = useWss;
+exports.newClient = newClient;
+exports.getMessageVersioinByRank = getMessageVersioinByRank;
+const types_1 = require("./types");
+// import { v4 as uuid } from "uuid";
+const socket_impl_1 = require("./socket_impl");
+function uuid() {
+    return `${Date.now()}${Math.random()}`;
+}
+// ============================================================ //
+// 全局配置
+// ============================================================ //
+// 站点
+let site = "47.57.236.213:8849";
+// 使用HTTPS
+let enableHttps = false;
+// 使用WSS
+let enableWss = false;
+// 设置站点
+function configSite(url) {
+    site = url;
+}
+// 使用HTTPS
+function useHttps() {
+    enableHttps = true;
+}
+// 使用WSS
+function useWss() {
+    enableWss = true;
+}
+function getBasicWebsocketUrl() {
+    if (enableWss) {
+        return `wss://${site}`;
+    }
+    else {
+        return `ws://${site}`;
+    }
+}
+function getBasicHttpUrl() {
+    if (enableHttps) {
+        return `https://${site}`;
+    }
+    else {
+        return `http://${site}`;
+    }
+}
+class RequestInfo {
+    constructor(config, interval, isIncrData = true) {
+        const now = Date.now();
+        this.nextRequestTime = now;
+        this.interval = interval;
+        this.config = config;
+        this.isIncrData = isIncrData;
+    }
+}
+exports.RequestInfo = RequestInfo;
+class ClientProvider {
+    constructor(eventHandle, token, showLog = false) {
+        this.socket = null;
+        this.url = getBasicWebsocketUrl() + "/ws";
+        this.token = token;
+        this.lastReqTime = 0;
+        this.lastRpsTime = 0;
+        this.autoConn = true;
+        this.isRunning = false;
+        this.interval = null;
+        this.callback = eventHandle;
+        this.requests = [];
+        this.showLog = showLog;
+    }
+    start() {
+        this.stop();
+        const now = Date.now();
+        this.lastReqTime = now;
+        this.lastRpsTime = now;
+        this.socket = new socket_impl_1.WebFuket(this.url, this.token);
+        this.socket.onopen = this.onOpen.bind(this);
+        this.socket.onclose = this.onClose.bind(this);
+        this.socket.onmessage = this.onMessage.bind(this);
+        this.socket.onerror = this.onError.bind(this);
+        this.interval = setInterval(this.handle.bind(this), 100);
+        this.requests.forEach((item) => {
+            item.nextRequestTime = now;
+        });
+        return this;
+    }
+    stop(autoConn = true) {
+        if (this.socket) {
+            this.socket.close();
+            this.socket = null;
+            if (this.showLog)
+                console.error("Websocket已断开");
+            this.isRunning = false;
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+        this.autoConn = autoConn;
+        return this;
+    }
+    registerChannel(config, interval, isIncrData) {
+        this.requests.push(new RequestInfo(config, interval, isIncrData));
+    }
+    enterRoom(roomId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // 订阅房间详情
+            this.registerChannel({
+                channel: types_1.ChannelType.RoomDetail,
+                version: "1.0",
+                seq: "0",
+                ts: Date.now(),
+                uid: uuid(),
+                params: { roomId }
+            }, 1500, false);
+            // 订阅房间团购详情
+            const roomGroupBuyingVersion = yield getMessageVersioinByRank(types_1.ChannelType.RoomGroupBuying, 1, { roomId }, this.token);
+            if (this.showLog) {
+                console.log(`订阅房间团购详情: roomId(${roomId}), 版本号(${roomGroupBuyingVersion})`);
+            }
+            this.registerChannel({
+                channel: types_1.ChannelType.RoomGroupBuying,
+                version: "1.0",
+                seq: roomGroupBuyingVersion,
+                ts: Date.now(),
+                uid: uuid(),
+                params: { roomId }
+            }, 100);
+            // 订阅房间投票
+            const roomVoteVersion = yield getMessageVersioinByRank(types_1.ChannelType.RoomVote, 1, { roomId }, this.token);
+            if (this.showLog) {
+                console.log(`订阅房间投票: roomId(${roomId}), 版本号(${roomVoteVersion})`);
+            }
+            this.registerChannel({
+                channel: types_1.ChannelType.RoomVote,
+                version: "1.0",
+                seq: roomVoteVersion,
+                ts: Date.now(),
+                uid: uuid(),
+                params: { roomId }
+            }, 100);
+            // 订阅房间消息
+            const roomMessageVersion = yield getMessageVersioinByRank(types_1.ChannelType.RoomMessage, 1, { roomId }, this.token);
+            if (this.showLog) {
+                console.log(`订阅房间消息: roomId(${roomId}), 版本号(${roomMessageVersion})`);
+            }
+            this.registerChannel({
+                channel: types_1.ChannelType.RoomMessage,
+                version: "1.0",
+                seq: roomMessageVersion,
+                ts: Date.now(),
+                uid: uuid(),
+                params: { roomId }
+            }, 100);
+            // 订阅房间用户消息
+            if (this.token) {
+                const roomUserMessageVersion = yield getMessageVersioinByRank(types_1.ChannelType.RoomUserMessage, 1, { roomId }, this.token);
+                if (this.showLog) {
+                    console.log(`订阅房间用户消息: roomId(${roomId}), 版本号(${roomUserMessageVersion})`);
+                }
+                this.registerChannel({
+                    channel: types_1.ChannelType.RoomUserMessage,
+                    version: "1.0",
+                    seq: roomUserMessageVersion,
+                    ts: Date.now(),
+                    uid: uuid(),
+                    params: { roomId }
+                }, 100);
+            }
+            return this;
+        });
+    }
+    leaveRoom(roomId) {
+        this.requests = this.requests.filter((request) => !([
+            types_1.ChannelType.RoomMessage,
+            types_1.ChannelType.RoomDetail,
+            types_1.ChannelType.RoomGroupBuying,
+            types_1.ChannelType.RoomVote,
+            types_1.ChannelType.RoomUserMessage
+        ].includes(request.config.channel) &&
+            request.config.params.roomId === roomId));
+        return this;
+    }
+    onOpen() {
+        if (this.showLog)
+            console.log("Websocket已连接");
+        this.isRunning = true;
+    }
+    onClose() {
+        this.stop();
+        if (this.autoConn) {
+            setTimeout(() => {
+                this.start();
+            }, 1000);
+        }
+    }
+    onMessage(event) {
+        const now = Date.now();
+        this.lastRpsTime = now;
+        const responses = JSON.parse(event.data);
+        // if (this.showLog) console.log("Websocket收到消息:", responses);
+        for (const response of responses) {
+            const request = this.requests.find((request) => request.config.uid === response.uid);
+            if (!request)
+                continue;
+            if (request.isIncrData) {
+                const currSeq = BigInt(request.config.seq);
+                const currRpsSeq = BigInt(response.rpsSeq);
+                if (currSeq >= currRpsSeq)
+                    continue;
+            }
+            switch (response.channel) {
+                case types_1.ChannelType.RoomDetail:
+                    for (const message of response.data) {
+                        this.callback.OnRoomDetail(this, request.config.params, message);
+                    }
+                    break;
+                case types_1.ChannelType.RoomGroupBuying:
+                    for (const message of response.data) {
+                        this.callback.OnRoomGroupBuying(this, request.config.params, message);
+                    }
+                    break;
+                case types_1.ChannelType.RoomMessage:
+                    for (const message of response.data) {
+                        switch (message.serviceType) {
+                            case types_1.ServiceType.RoomGroupBuyingNextProduct:
+                                this.callback.OnRoomGroupBuyingNextProduct(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingStart:
+                                this.callback.OnRoomGroupBuyingStart(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingLotteryOpening:
+                                this.callback.OnRoomGroupBuyingLotteryOpening(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingWinning:
+                                this.callback.OnRoomGroupBuyingWinning(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingBiddingCounteroffer:
+                                this.callback.OnRoomGroupBuyingBiddingCounteroffer(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingBiddingDeal:
+                                this.callback.OnRoomGroupBuyingBiddingDeal(this, request.config.params, message);
+                                break;
+                        }
+                    }
+                    break;
+                case types_1.ChannelType.RoomVote:
+                    for (const message of response.data) {
+                        this.callback.OnRoomGroupBuyingVote(this, request.config.params, message);
+                    }
+                    break;
+                case types_1.ChannelType.RoomUserMessage:
+                    for (const message of response.data) {
+                        switch (message.serviceType) {
+                            case types_1.ServiceType.RoomGroupBuyingBiddingBuyerInitiatesOffer:
+                                this.callback.OnRoomGroupBuyingBiddingBuyerInitiatesOffer(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingBiddingSellerReceivesOffer:
+                                this.callback.OnRoomGroupBuyingBiddingSellerReceivesOffer(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingBiddingSellerCounteroffer:
+                                this.callback.OnRoomGroupBuyingBiddingSellerCounteroffer(this, request.config.params, message);
+                                break;
+                            case types_1.ServiceType.RoomGroupBuyingBiddingBuyerOfferRejected:
+                                this.callback.OnRoomGroupBuyingBiddingBuyerOfferRejected(this, request.config.params, message);
+                                break;
+                        }
+                    }
+                    break;
+            }
+            request.config.seq = response.rpsSeq;
+        }
+    }
+    onError(event) {
+        if (this.showLog)
+            console.error("Websocket连接出现错误:", event);
+    }
+    isTimeout() {
+        const now = Date.now();
+        if (this.lastReqTime + 15000 > now)
+            return false;
+        if (this.lastRpsTime + 15000 > now)
+            return false;
+        return true;
+    }
+    handle() {
+        var _a;
+        if (!this.isRunning)
+            return;
+        if (this.isTimeout()) {
+            if (this.showLog)
+                console.error("连接超时");
+            this.stop();
+            return;
+        }
+        if (this.requests.length == 0)
+            return;
+        const now = Date.now();
+        const requests = [];
+        for (const request of this.requests) {
+            if (request.nextRequestTime > now)
+                continue;
+            request.nextRequestTime = now + request.interval;
+            requests.push(request.config);
+        }
+        // if (this.showLog) console.log("Websocket发送消息:", requests);
+        (_a = this.socket) === null || _a === void 0 ? void 0 : _a.send(JSON.stringify(requests));
+        this.lastReqTime = now;
+    }
+}
+exports.ClientProvider = ClientProvider;
+function newClient(eventHandle, token, showLog) {
+    return new ClientProvider(eventHandle, token, showLog);
+}
+function getMessageVersioinByRank(channel_1) {
+    return __awaiter(this, arguments, void 0, function* (channel, rank = 1, params = {}, token) {
+        const headers = {
+            "Content-Type": "application/json"
+        };
+        if (token)
+            headers["token"] = token;
+        const url = new URL(getBasicHttpUrl() + "/getMessageVersioinByRank");
+        url.search = new URLSearchParams(Object.assign(params, { channel, rank })).toString();
+        if (process.env.UNI_PLATFORM === "app-plus") {
+            return new Promise((resolve, reject) => {
+                //@ts-ignore
+                uni.request({
+                    url: url.toString(),
+                    header: headers,
+                    success: (res) => {
+                        resolve(res.data);
+                    },
+                    fail: () => {
+                        reject();
+                    }
+                });
+            });
+        }
+        else {
+            return fetch(url.toString(), {
+                method: "GET",
+                headers
+            })
+                .then((res) => res.json())
+                .then((json) => json.data);
+        }
+    });
+}
+//# sourceMappingURL=socket.js.map
