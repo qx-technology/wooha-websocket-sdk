32b0127d6aea1a57751d328a7c1545cb3182682d
Husky <husky.robot.dog@gmail.com>
2024-08-12 15:41:06 +0800

    coding

--- src/types.ts
+++ src/types.ts
@@ -182,150 +182,264 @@ export interface RoomDetail {
   onlinePeople: bigint;
 }
 
-/// 房间团购详情
+/**
+ * 房间团购详情
+ */
 export interface RoomGroupBuying {
-  /// 房间ID
-  id: string;
-  /// 团购ID
-  groupBuyingId: number;
-  /// 最大可投票数
-  maxVoteTickets: number;
-  /// 用户最大可投票数
-  userMaxVoteTickets: number;
-  /// 当前已投票数
-  currentVoteTickets: number;
-  /// 投票进度
-  voteProgress: number;
+  /**
+   * 房间ID
+   */
+  id: bigint;
+  /**
+   * 团购ID
+   */
+  groupBuyingId: bigint;
+  /**
+   * 最大投票数
+   */
+  maxVoteTickets: bigint;
+  /**
+   * 用户最大投票数
+   */
+  userMaxVoteTickets: bigint;
+  /**
+   * 当前投票数
+   */
+  currentVoteTickets: bigint;
+  /**
+   * 投票进度
+   */
+  voteProgress: bigint;
 }
 
-/// 房间团购投票
+/**
+ * 房间团购投票
+ */
 export interface RoomGroupBuyingVote {
-  /// 团购ID
-  groupBuyingId: number;
-  /// 用户ID
-  userId: string;
-  /// 用户名
+  /**
+   * 团购ID
+   */
+  groupBuyingId: bigint;
+  /**
+   * 用户ID
+   */
+  userId: bigint;
+  /**
+   * 用户名
+   */
   username: string;
-  /// 用户头像
+  /**
+   * 用户头像
+   */
   userAvatar: string;
-  /// 用户头像框
+  /**
+   * 用户头像框
+   */
   userAvatarFrame: string;
-  /// 投票时间
-  voteTime: string;
-  /// 投票数
-  tickets: number;
+  /**
+   * 投票时间
+   */
+  voteTime: number;
+  /**
+   * 票数
+   */
+  tickets: bigint;
 }
 
-/// 房间团购下一个商品
+/**
+ * 房间团购下一个商品
+ */
 export interface RoomGroupBuyingNextProduct {
-  /// 团购ID
-  groupBuyingId: string;
-  /// 商品ID
-  productId: string;
-  /// 商品名
+  // 团购ID
+  groupBuyingId: bigint;
+  // 商品ID
+  productId: bigint;
+  // 商品名称
   productName: string;
-  /// 商品图片
+  // 商品图片
   productImage: string;
-  /// SKUID
-  skuId: string;
-  /// 开始时间
-  beginTime: string;
+  // SKUID
+  skuId: bigint;
+  // 开始时间
+  beginTime: number;
 }
 
-/// 房间团购开始
+/**
+ * 房间团购开始
+ */
 export interface RoomGroupBuyingStart {}
 
-/// 房间团购正在开奖
+/**
+ * 房间团购正在开奖
+ */
 export interface RoomGroupBuyingLotteryOpening {}
 
-/// 房间团购中奖
+/**
+ * 房间团购中奖
+ */
 export interface RoomGroupBuyingWinning {
-  /// 中奖用户ID
-  winnerUserId: string;
-  /// 中奖用户名
-  winnerUsername: string;
-  /// 中奖用户头像
+  /**
+   * 中奖用户ID
+   */
+  winnerUserId: bigint;
+  /**
+   * 中奖用户名
+   */
+  winnerUserName: string;
+  /**
+   * 中奖用户头像
+   */
   winnerUserAvatar: string;
-  /// 中奖用户头像框
+  /**
+   * 中奖用户头像框
+   */
   winnerUserAvatarFrame: string;
-  /// 奖品可领奖时间
-  prizeCollectionTime: string;
-  /// 商品ID
-  productId: string;
-  /// 商品名
+  /**
+   * 奖品领取时间
+   */
+  prizeCollectionTime: number;
+  /**
+   * 商品ID
+   */
+  productId: bigint;
+  /**
+   * 商品名
+   */
   productName: string;
-  /// 商品图片
+  /**
+   * 商品图片
+   */
   productImage: string;
-  /// SKUID
-  skuId: string;
-  /// 竞拍ID
-  auctionId: string;
+  /**
+   * SKUID
+   */
+  skuId: bigint;
+  /**
+   * 竞拍ID
+   */
+  auctionId: bigint;
 }
 
-/// 房间团购竞拍还价所有人
+/**
+ * 房间团购竞拍还价所有人
+ */
 export interface RoomGroupBuyingBiddingCounteroffer {
-  /// 竞拍ID
-  auctionId: string;
-  /// 还价金额
-  amount: number;
+  /**
+   * 竞拍ID
+   */
+  auctionId: bigint;
+  /**
+   * 金额
+   */
+  amount: bigint;
 }
 
-/// 房间团购竞拍成交
+/**
+ * 房间团购竞拍成交
+ */
 export interface RoomGroupBuyingBiddingDeal {
-  /// 竞拍ID
-  auctionId: string;
-  /// 出价用户ID
-  userId: string;
-  /// 金额
-  amount: number;
-  /// 卖方用户id
-  sellerUserId: string;
-  /// 卖方用户名
+  /**
+   * 竞拍ID
+   */
+  auctionId: bigint;
+  /**
+   * 出价用户ID
+   */
+  userId: bigint;
+  /**
+   * 金额
+   */
+  amount: bigint;
+  /**
+   * 卖家用户ID
+   */
+  sellerUserId: bigint;
+  /**
+   * 卖家用户名
+   */
   sellerUsername: string;
-  /// 卖方用户头像
+  /**
+   * 卖家用户头像
+   */
   sellerUserAvatar: string;
-  /// 卖方用户头像框
+  /**
+   * 卖家用户头像框
+   */
   sellerUserAvatarFrame: string;
-  /// 商品名称
+  /**
+   * 商品名
+   */
   productName: string;
-  /// 商品图片
+  /**
+   * 商品图片
+   */
   productImage: string;
 }
 
-/// 房间团购竞拍买家发起报价(私人)
+/**
+ * 房间团购竞拍买家发起报价(私人)
+ */
 export interface RoomGroupBuyingBiddingBuyerInitiatesOffer {
-  /// 竞拍ID
-  auctionId: string;
-  /// 出价用户ID
-  userId: string;
-  /// 金额
-  amount: number;
+  /**
+   * 竞拍ID
+   */
+  auctionId: bigint;
+  /**
+   * 出价用户ID
+   */
+  userId: bigint;
+  /**
+   * 金额
+   */
+  amount: bigint;
 }
 
-/// 房间团购竞拍卖家收到报价(私人)
+/**
+ * 房间团购竞拍卖家收到报价(私人)
+ */
 export interface RoomGroupBuyingBiddingSellerReceivesOffer {
-  /// 竞拍ID
-  auctionId: string;
-  /// 出价用户ID
-  userId: string;
-  /// 金额
-  amount: number;
+  /**
+   * 竞拍ID
+   */
+  auctionId: bigint;
+  /**
+   * 出价用户ID
+   */
+  userId: bigint;
+  /**
+   * 金额
+   */
+  amount: bigint;
 }
 
-/// 房间团购竞拍买家收到还价(私人)
+/**
+ * 房间团购竞拍买家收到还价(私人)
+ */
 export interface RoomGroupBuyingBiddingSellerCounteroffer {
-  /// 竞拍ID
-  auctionId: string;
-  /// 金额
-  amount: number;
+  /**
+   * 竞拍ID
+   */
+  auctionId: bigint;
+  /**
+   * 金额
+   */
+  amount: bigint;
 }
 
-/// 房间团购竞拍买家报价被拒(私人)
+/**
+ * 房间团购竞拍买家报价被拒(私人)
+ */
 export interface RoomGroupBuyingBiddingBuyerOfferRejected {
-  /// 竞拍ID
-  auctionId: string;
-  /// 出价用户ID
-  userId: string;
-  /// 金额
-  amount: number;
+  /**
+   * 竞拍ID
+   */
+  auctionId: bigint;
+  /**
+   * 出价用户ID
+   */
+  userId: bigint;
+  /**
+   * 金额
+   */
+  amount: bigint;
 }
